19190a55ad6a96ecb83ea1463b757b8f697b3325
CamCove-Crichton <>
2023-05-18 05:15:56 +0000

    Remove secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    'SECRET_KEY', 'django-insecure-sa$97218k49isflal_c404($l3d$!u5-x-2psli7l(wzo(ysgi')
+    'SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
